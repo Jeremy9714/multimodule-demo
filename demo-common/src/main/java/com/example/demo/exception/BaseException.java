5fa0b815400b219d899e0246d5b59476bec8116b
zhangchy05 <zhangchy05@inspur.com>
2024-05-29 16:47:47 +0800

    异常处理

---
+++ demo-common/src/main/java/com/example/demo/exception/BaseException.java
@@ -0,0 +1,45 @@
+package com.example.demo.exception;
+
+import lombok.Data;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Description:
+ * @Author: zhangchy05 on 2024/5/16 17:12
+ * @Version: 1.0
+ */
+@Data
+public class BaseException extends RuntimeException {
+    private static final long serialVersionUID = 1998063243843477017L;
+    private static final Logger logger = LoggerFactory.getLogger(BaseException.class);
+    private int errorCode;
+//    private String errorView;
+    private String errorMessage;
+
+    public BaseException(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public BaseException(int errorCode, String errorMessage) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        if (logger.isDebugEnabled()) {
+            logger.debug("{code:" + errorCode + ",msg:" + errorMessage + "}");
+        }
+    }
+
+//    public BaseException(String errorView, int errorCode, String errorMessage) {
+//        if (StringUtils.isNotEmpty(errorView)) {
+//            throw new IllegalArgumentException("The parameter errorView can not be blank.");
+//        } else {
+//            this.errorCode = errorCode;
+//            this.errorView = errorView;
+//            this.errorMessage = errorMessage;
+//            if (logger.isDebugEnabled()) {
+//                logger.debug("{code:" + errorCode + ",view:" + errorView + ",msg:" + errorMessage + "}");
+//            }
+//        }
+//    }
+}
---
+++ demo-common/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.demo.exception;
+
+import com.alibaba.druid.support.json.JSONUtils;
+import com.example.demo.constant.es.AjaxResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.RequestContextHolder;
+import cn.hutool.core.util.ObjectUtil;
+
+/**
+ * @Description:
+ * @Author: zhangchy05 on 2024/5/16 17:21
+ * @Version: 1.0
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BaseException.class)
+    public AjaxResult<?> commonException(BaseException be) {
+        Object requestId = RequestContextHolder.getRequestAttributes()
+                .getAttribute("requestId", 0);
+        log.error("异常：requestId[{}]，错误信息：{}", requestId, JSONUtils.toJSONString(be));
+        return new AjaxResult<>(be.getErrorCode(), be.getErrorMessage(), null);
+    }
+}
